38d9bfb0b51195c2bde1e4afbeeb187a750e8c7e
Mohamedboukerche22 <mohamedboukerche55@gmail.com>
2025-01-01 22:58:31 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function goToNextPage() {
+    document.getElementById('welcomePage').style.display = 'none';
+    document.getElementById('environmentPage').style.display = 'block';
+}
+
+function goBack() {
+    document.getElementById('environmentPage').style.display = 'none';
+    document.getElementById('welcomePage').style.display = 'block';
+}
